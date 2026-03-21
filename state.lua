5f52ad992c9beb05d95e1b4867e585bf42675d73
kboltiz <kazabite.music@gmail.com>
2026-03-21 17:02:31 +0000

    Fixes for Round 3
    - Moved global variables to a state.lua table, which is now global instead.
    - Update drawing logic to scales, completely.
    - play_sound makes no sense, removed it.
    - Separated player movement smoothing from the shake logic, it shouldn't have been there at all.

--- state.lua
+++ state.lua
@@ -1,63 +1,24 @@
--- more magic numbers (probably could simplify with %)
+local M = {}
 
-ROW_X            = { 60, 135, 210 }
-SPAWN_Y          = -30
-FLOOR_Y          = 330
-PLAYER_Y         = 355
-BLOCK            = 30
-VISUAL_X_SPEED   = 0.2
-DROID_SPEED_INIT = 0.08
+M.ROW_X            = { 0.2, 0.45, 0.7 }
+M.SPAWN_Y          = -0.075
+M.FLOOR_Y          = 0.825
+M.PLAYER_Y         = 0.8875
+M.BLOCK_W          = 0.1
+M.BLOCK_H          = 0.075
+M.VISUAL_X_SPEED   = 0.2
+M.DROID_SPEED_INIT = 0.0001
 
-DOT_COLORS = {
+M.DOT_COLORS = {
     top    = { r=255, g=220, b=50  },
     bottom = { r=50,  g=255, b=150 },
     left   = { r=50,  g=150, b=255 },
     right  = { r=255, g=100, b=200 },
 }
+M.DOT_SIDES = { 'top', 'bottom', 'left', 'right' }
 
-DOT_SIDES = { 'top', 'bottom', 'left', 'right' }
-
-player_row    = 2
-visual_x      = 135
-lives         = 3
-score         = 0
-droids_killed = 0
-shake_frames  = 0
-droid_speed   = DROID_SPEED_INIT
-
--- all above, could return as a table
---[[
-
--- here
-local M = {}
-...                     -- fills M
-return M
-
--- main
-val G = require "local" -- G=M
-]]
-
-
-function check_speed_bump ()
-    if droids_killed > 0 and droids_killed % 5 == 0 then
-        droid_speed = droid_speed + 0.01
-    end
-end
-
--- just call output.sound there (simplify!)
--- why all this checks?
-function play_sound (path)
-    local f = io.open(path, "r")
-    if f then
-        f:close()
-        pico.output.sound(path)
-    else
-        print("SOUND NOT FOUND: " .. path)
-    end
-end
-
-function shuffle_sides ()
-    local t = { table.unpack(DOT_SIDES) }
+function M.shuffle_sides ()
+    local t = { table.unpack(M.DOT_SIDES) }
     for i = #t, 2, -1 do
         local j = math.random(i)
         t[i], t[j] = t[j], t[i]
@@ -65,7 +26,7 @@ function shuffle_sides ()
     return t
 end
 
-function fmt_time (ms)
+function M.fmt_time (ms)
     local total_s = math.floor(ms / 1000)
     local mins    = math.floor(total_s / 60)
     local secs    = total_s % 60
@@ -73,3 +34,5 @@ function fmt_time (ms)
     local ss      = secs < 10 and ("0" .. secs) or ("" .. secs)
     return mm .. ":" .. ss
 end
+
+return M
